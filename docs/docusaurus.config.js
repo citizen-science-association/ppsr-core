835a8cf7bc4ccd83085e16ffc035915922d6995a
BBudnicki <50023776+BBudnicki@users.noreply.github.com>
2020-08-18 21:10:37 -0500

    updated docs and blog url edit

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -105,13 +105,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/BBudnicki/docusaurus/edit/master/website/',
+            'https://github.com/BBudnicki/docusaurus/edi/master/docs/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/BBudnicki/docusaurus/edit/master/website/blog/',
+          'https://github.com/BBudnicki/docusaurus/edi/master/docs/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
