91b326b357075cc428d9a3782d9dce23b40a1690
BBudnicki <50023776+BBudnicki@users.noreply.github.com>
2020-08-18 22:15:01 -0500

    added margins to schema demo

--- src/pages/schema/index.js
+++ src/pages/schema/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import SchemaLayout from '../../components/schema'
+import React from "react";
+import Layout from "@theme/Layout";
+import SchemaLayout from "../../components/schema";
 function SchemaPage() {
   return (
     <Layout title="Schema">
-      <h1>Schema Demo</h1>
-      <p>This page contains content loaded from the schema.org JSON-LD file. 
-        It automaticly parses and displays it on the page. 
-        This can be shown in raw form or be used to fill in placeholders on the page.</p>
+      <div className="container">
+        <div className="padding-vert--lg">
+          <h1>Schema Demo</h1>
+          <p>
+            This page contains content loaded from the schema.org JSON-LD file.
+            It automaticly parses and displays it on the page. This can be shown
+            in raw form or be used to fill in placeholders on the page.
+          </p>
+        </div>
+      </div>
+
       <SchemaLayout label="Airline"></SchemaLayout>
     </Layout>
   );
