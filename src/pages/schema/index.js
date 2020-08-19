2c1d0f68840c8b088cbe5bb443cab42f871fedcb
BBudnicki <50023776+BBudnicki@users.noreply.github.com>
2020-08-18 22:11:36 -0500

    schema page demo

--- src/pages/schema/index.js
+++ src/pages/schema/index.js
@@ -4,6 +4,10 @@ import SchemaLayout from '../../components/schema'
 function SchemaPage() {
   return (
     <Layout title="Schema">
+      <h1>Schema Demo</h1>
+      <p>This page contains content loaded from the schema.org JSON-LD file. 
+        It automaticly parses and displays it on the page. 
+        This can be shown in raw form or be used to fill in placeholders on the page.</p>
       <SchemaLayout label="Airline"></SchemaLayout>
     </Layout>
   );
