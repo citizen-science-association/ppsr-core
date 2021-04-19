b48538af66152b676c0e8bb72e2bdacbaed12d29
BBudnicki <50023776+BBudnicki@users.noreply.github.com>
2021-04-18 21:24:23 -0500

    force https beta

--- docusaurus.config.dev.js
+++ docusaurus.config.dev.js
@@ -3,7 +3,7 @@ let docusaurusDEV = {
   ...docusaurusPRD,
   title: `BETA ${docusaurusPRD.title}`,
   noIndex: true, // Defaults to `false`
-  url: "http://bbudnicki.github.io",
+  url: "https://bbudnicki.github.io",
   baseUrl: '/ppsr-core/',
   organizationName: 'bbudnicki',
 };
